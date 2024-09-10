6d1d2d9d21860354a67b69dae77c44914e4c808f
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2024-09-10 20:52:32 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-
-import {React , useEffect} from 'react';
-import AOS from 'aos';
-import { Route, Routes } from 'react-router-dom';
-
-import routes from './pages';
-import Page404 from './pages/404';
-import Header from './components/header/Header';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-    useEffect(() => {
-        AOS.init({
-          duration : 2000
-        }); 
-      }, []);
-
-    return (
-        <>
-
-            <Header />
-
-            <Routes>
-
-                {
-                    routes.map((data,idx) => (
-                        <Route key={idx} path={data.path} element={data.component} exact />
-                    ))
-                }
-
-                <Route path='*' element={<Page404 />} />
-            </Routes>
-
-    
-        </>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
