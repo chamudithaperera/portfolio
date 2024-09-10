b1dc322d4abba522d6b7a352521727a1167778fa
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2024-09-10 22:53:19 +0530

    commit7

--- src/App.js
+++ src/App.js
@@ -9,19 +9,13 @@ function App() {
         <h1>
         ChamXDev is Coming Soon!
         </h1>
-        <h3>
-        Thank you for visiting. We're currently working hard to build something amazing for you. Stay tuned for our launch!<br></br>
-
-        In the meantime, if you have any questions or need to get in touch, feel free to
-        </h3>
+       
         <a
-          className="Contact us"
+          className="App-link"
           href="https://www.facebook.com/chamuditha.kavishan.1"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
           Learn React
         </a>
       </header>
