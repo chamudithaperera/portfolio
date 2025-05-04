fabe8176741180f80375ea0d1d58314905e6d13e
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-05-04 08:29:03 +0530

    Add framer-motion dependency and update experience details in experience.json. Refactor App.js and Home.js for improved structure and maintainability. Simplify App.css styles and enhance Navbar component in dark theme.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,46 +22,46 @@ import Experience from '../components/dark/home/experience';
 
 function Home() {
   return (
-    <div className="app-container">
+    <div>
       <Helmet>
-        <title>ChamXdev | Portfolio</title>
+        <title>ChamXdev</title>
         <link rel="icon" href="/assets/imgs/favicon.ico" />
         <link rel="shortcut icon" href="/assets/imgs/favicon.ico" />
         <link rel="stylesheet" type="text/css" href="/assets/css/plugins.css" />
         <link rel="stylesheet" type="text/css" href="/assets/css/style.css" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Helmet>
-      
       <Cursor />
       <ContactUs />
       <Lines />
       <LoadingScreen />
       <ProgressScroll />
-      
-      <div className="main-content">
+      <div>
         <NavTop />
         <main className="container">
-          <div className="content-wrapper">
-            <Profile />
-            <Navbar />
-            <section className="in-box fade-in">
-              <Services />
-              {/* <Skills /> */}
-              <Portfolio />
-              <Experience />
-              <Testimonials />
-              {/* <AddReview /> */}
-              {/* <Price /> */}
-              <Info />
-              <Blog />
-            </section>
-          </div>
+          <Profile />
+          <Navbar />
+          <section className="in-box">
+            <Services />
+            {/* <Skills /> */}
+            <Portfolio />
+            <Experience />
+            <Testimonials />
+            {/* <AddReview /> */}
+            {/* <Price /> */}
+            <Info />
+            <Blog />
+          </section>
         </main>
         <Footer />
       </div>
-      
-      <script src="/assets/js/jquery-3.6.0.min.js" strategy="beforeInteractive" />
-      <script src="/assets/js/jquery-migrate-3.4.0.min.js" strategy="beforeInteractive" />
+      <script
+        src="/assets/js/jquery-3.6.0.min.js"
+        strategy="beforeInteractive"
+      />
+      <script
+        src="/assets/js/jquery-migrate-3.4.0.min.js"
+        strategy="beforeInteractive"
+      />
       <script src="/assets/js/plugins.js" strategy="lazyOnload" />
       <script src="/assets/js/scripts.js" strategy="beforeInteractive" />
       <script src="/assets/js/three.min.js" strategy="lazyOnload" />
