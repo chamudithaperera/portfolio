cf1bf2288b2d9c8a73c0f979729907989a7fbb26
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-05-04 08:13:33 +0530

    Enhance styling and layout in App.css and Home.js. Added CSS variables for theming, improved responsiveness, and modernized component styles. Updated Home.js structure for better layout and added fade-in animation for sections.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,46 +22,46 @@ import Experience from '../components/dark/home/experience';
 
 function Home() {
   return (
-    <div>
+    <div className="app-container">
       <Helmet>
-        <title>ChamXdev</title>
+        <title>ChamXdev | Portfolio</title>
         <link rel="icon" href="/assets/imgs/favicon.ico" />
         <link rel="shortcut icon" href="/assets/imgs/favicon.ico" />
         <link rel="stylesheet" type="text/css" href="/assets/css/plugins.css" />
         <link rel="stylesheet" type="text/css" href="/assets/css/style.css" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Helmet>
+      
       <Cursor />
       <ContactUs />
       <Lines />
       <LoadingScreen />
       <ProgressScroll />
-      <div>
+      
+      <div className="main-content">
         <NavTop />
         <main className="container">
-          <Profile />
-          <Navbar />
-          <section className="in-box">
-            <Services />
-            {/* <Skills /> */}
-            <Portfolio />
-            <Experience />
-            <Testimonials />
-            {/* <AddReview /> */}
-            {/* <Price /> */}
-            <Info />
-            <Blog />
-          </section>
+          <div className="content-wrapper">
+            <Profile />
+            <Navbar />
+            <section className="in-box fade-in">
+              <Services />
+              {/* <Skills /> */}
+              <Portfolio />
+              <Experience />
+              <Testimonials />
+              {/* <AddReview /> */}
+              {/* <Price /> */}
+              <Info />
+              <Blog />
+            </section>
+          </div>
         </main>
         <Footer />
       </div>
-      <script
-        src="/assets/js/jquery-3.6.0.min.js"
-        strategy="beforeInteractive"
-      />
-      <script
-        src="/assets/js/jquery-migrate-3.4.0.min.js"
-        strategy="beforeInteractive"
-      />
+      
+      <script src="/assets/js/jquery-3.6.0.min.js" strategy="beforeInteractive" />
+      <script src="/assets/js/jquery-migrate-3.4.0.min.js" strategy="beforeInteractive" />
       <script src="/assets/js/plugins.js" strategy="lazyOnload" />
       <script src="/assets/js/scripts.js" strategy="beforeInteractive" />
       <script src="/assets/js/three.min.js" strategy="lazyOnload" />
