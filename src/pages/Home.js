159696de4efb9026cf94f4b7d3a16b0237f35566
Chamuditha Kavishan Perera <139614066+chamudithaperera@users.noreply.github.com>
2026-08-14 16:30:18 +0530

    Reduce solar system radius for improved layout fit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1960,7 +1960,7 @@ function SolarSystem({ running, selectedStack, onSelectStack }) {
     }
 
     const measure = () => {
-      const radiusFactor = window.innerWidth < 768 ? 0.4 : 0.48;
+      const radiusFactor = window.innerWidth < 768 ? 0.36 : 0.43;
       setRadius(Math.min(node.clientWidth, node.clientHeight) * radiusFactor);
     };
     measure();
