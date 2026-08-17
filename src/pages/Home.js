d1f72d94bd19f3b5309fd11fd95fe97445efb1df
Chamuditha Kavishan Perera <139614066+chamudithaperera@users.noreply.github.com>
2026-08-17 15:50:31 +0530

    Add useNavigate to Home page routing imports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { Helmet } from 'react-helmet';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import {
   motion,
   AnimatePresence,
